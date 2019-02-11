13ab549a0cfbc8286bc5db6c55a0e3bd8152d671
Douglas Tan <bianster@gmail.com>
2019-02-11 19:21:48 +0800

    Configure barebones setup.py for instalation.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="pyms",
+    version="1.0.0",
+    author_email="bianster@gmail.com",
+    description="Read Metastock data in Python",
+    py_modules = ['pyms']
+)
